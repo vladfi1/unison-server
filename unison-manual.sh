928b0b7075ab864a9e9e3240e70fa9e7dccc8f02
vladfi1 <vladfi2@gmail.com>
2014-09-16 20:42:28 -0400

    Pull server ip.

--- unison-manual.sh
+++ unison-manual.sh
@@ -1,5 +1,8 @@
+cd $HOME/.homesick/repos/serverip
+git pull
+
 options="-ui text"
 local="-root $HOME"
-serverip=$(cat $HOME/.homesick/repos/serverip/ip.txt)
+serverip=$(cat ip.txt)
 remote="-root ssh://$serverip"
 unison ignore $options $local $remote -path $1
