3b6dce880297842e7b2d3565a4e698abf3c7bc94
vladfi1 <vladfi2@gmail.com>
2015-05-17 18:20:09 -0400

    Complete setup script.

--- setup-client.sh
+++ setup-client.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# should automate ssh stuff?
+
+# homeshick
+git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
+printf '\nsource "$HOME/.homesick/repos/homeshick/homeshick.sh"' >> $HOME/.bashrc
+printf '\nsource "$HOME/.homesick/repos/homeshick/completions/homeshick-completion.bash"' >> $HOME/.bashrc
+
+# serverip on bitbucket
+homeshick clone git@bitbucket.org:vladfi1/serverip.git
+
+# clone this repo (unison-server)
+homeshick clone git@github.com:vladfi1/unison-server.git
+
 # install unison
 
 if [ -d /etc/apt ]; then
@@ -13,18 +26,13 @@ fi
 INSTALL="sudo $INSTALL -y install"
 $INSTALL unison
 
-#export UNISONLOCALHOSTNAME=$USER@$HOSTNAME
-#echo $UNISONLOCALHOSTNAME
-
-cd $HOME
-
 # make unison profile
 
-if [ ! -d .unison ]; then
-	mkdir .unison
+if [ ! -d $HOME/.unison ]; then
+	mkdir $HOME/.unison
 fi
 
-cat <<EOF > .unison/ignore
+cat <<EOF > $HOME/.unison/ignore
 ignore = Name *~
 ignore = Name .~*
 
@@ -36,14 +44,16 @@ ignore = Name .dropbox*
 ignore = Path .SyncTrash/*
 EOF
 
-echo ignore > .unison/ignore.prf
+echo ignore > $HOME/.unison/ignore.prf
 
-# serverip on bitbucket
-# should automate ssh stuff?
-git clone git@bitbucket.org:vladfi1/serverip.git
+# unison aliases
+echo "alias ssh-server=$HOME/.homesick/repos/unison-server/ssh.sh" >> $HOME/.bashrc
+echo "alias sync-manual=$HOME/.homesick/repos/unison-server/unison-manual.sh" >> $HOME/.bashrc
+echo "alias sync-folder=$HOME/.homesick/repos/unison-server/unison.sh" >> $HOME/.bashrc
+echo "alias sync-common=\"sync-folder common\"" >> $HOME/.bashrc
 
-# setup crontab
-prevtab=$(crontab -l)
+echo "export UNISONLOCALHOSTNAME=$USER@$HOSTNAME" >> $HOME/.bashrc
 
+source $HOME/.bashrc
+echo $UNISONLOCALHOSTNAME
 
-(crontab -l; cat common/scripts/server/clienttab.txt) | crontab -
