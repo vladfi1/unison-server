6a2fc6ccc0a2a4f3097bfdaa01b8781f80a03a4f
vladfi1 <vladfi2@gmail.com>
2015-05-17 18:38:24 -0400

    Correct homesick url.

--- setup-client.sh
+++ setup-client.sh
@@ -3,7 +3,7 @@
 # should automate ssh stuff?
 
 # homeshick
-git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
+git clone https://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 printf '\nsource "$HOME/.homesick/repos/homeshick/homeshick.sh"' >> $HOME/.bashrc
 printf '\nsource "$HOME/.homesick/repos/homeshick/completions/homeshick-completion.bash"' >> $HOME/.bashrc
 
